3eda53f256f81261d35c48cb6343f5d7e231ad34
Luana Melissa Linux <luanamelissaprogrammer@gmail.com>
2022-08-25 15:52:35 -0300

    Mapeando as requisições com Request Mapping

--- src/main/java/io/github/luaprogrammer/rest/controller/ClienteController.java
+++ src/main/java/io/github/luaprogrammer/rest/controller/ClienteController.java
@@ -10,7 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(value = {"/hello/{nome}", "api/helo"}, //o value pode receber um array de strings
+            method = RequestMethod.GET,
+            consumes = { "application/json", "application/xml" }, //o consumer vai falar que tipo de dados essa requisição vai receber, mais é colocado em requisições POST
+            produces = { "application/json", "application/xml" } //forma de retorno do Cliente
+    )
     @ResponseBody //diz que a string que vai retornar é o corpo da resposta
     public String helloCliente( @PathVariable("nome") String nomeCliente) {
         return String.format("Hello %s", nomeCliente);
