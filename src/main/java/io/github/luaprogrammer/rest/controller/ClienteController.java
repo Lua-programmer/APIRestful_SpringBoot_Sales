a7c6e7d75f6c9644391fcd3dc1e485b75ee0c624
Luana Melissa Linux <luanamelissaprogrammer@gmail.com>
2022-08-28 23:21:55 -0300

    Delete Mapping: deletando um recurso no servidor

--- src/main/java/io/github/luaprogrammer/rest/controller/ClienteController.java
+++ src/main/java/io/github/luaprogrammer/rest/controller/ClienteController.java
@@ -33,5 +33,16 @@ public class ClienteController {
         Cliente clienteSalvo = clientes.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
     }
+    @DeleteMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if (cliente.isPresent()) {
+            clientes.delete( cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
