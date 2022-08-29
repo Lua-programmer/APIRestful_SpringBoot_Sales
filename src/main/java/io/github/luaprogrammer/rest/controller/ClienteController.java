ebe1b19da373ffe0c51b606480cc5d5d8de6bd25
Luana Melissa Linux <luanamelissaprogrammer@gmail.com>
2022-08-28 23:49:08 -0300

    Put Mapping - Atualizando um cliente

--- src/main/java/io/github/luaprogrammer/rest/controller/ClienteController.java
+++ src/main/java/io/github/luaprogrammer/rest/controller/ClienteController.java
@@ -28,21 +28,32 @@ public class ClienteController {
     }
 
     @PostMapping("/api/clientes")
-    @ResponseBody  //Responde é o que retornar e Request é que entra na requisição
+    @ResponseBody  //Response é o que retornar e Request é que entra na requisição
     public ResponseEntity save(@RequestBody Cliente cliente) {
         Cliente clienteSalvo = clientes.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
     }
+
     @DeleteMapping("/api/clientes/{id}")
     @ResponseBody
     public ResponseEntity delete(@PathVariable Integer id) {
         Optional<Cliente> cliente = clientes.findById(id);
 
         if (cliente.isPresent()) {
-            clientes.delete( cliente.get());
+            clientes.delete(cliente.get());
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente) {
+        return clientes.findById(id).map(clienteExistente -> {
+            cliente.setId(clienteExistente.getId());
+            clientes.save(cliente);
+            return ResponseEntity.noContent().build();
+        }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
