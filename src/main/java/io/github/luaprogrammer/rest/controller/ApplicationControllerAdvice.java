41cf7cd13b0249e8b680580323c05ba026fe9bc2
Luana Melissa Linux <luanamelissaprogrammer@gmail.com>
2022-08-30 12:28:26 -0300

    Utilizando e testando o Bean Validation

--- src/main/java/io/github/luaprogrammer/rest/controller/ApplicationControllerAdvice.java
+++ src/main/java/io/github/luaprogrammer/rest/controller/ApplicationControllerAdvice.java
@@ -4,7 +4,11 @@ package io.github.luaprogrammer.rest.controller;
 import io.github.luaprogrammer.exception.PedidoNaoEncontradoException;
 import io.github.luaprogrammer.rest.ApiErrors;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -20,5 +24,12 @@ public class ApplicationControllerAdvice {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ApiErrors handlePedidoNotFoundException(PedidoNaoEncontradoException e) {
         return new ApiErrors(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException e) {
+        List<String> errors = e.getBindingResult().getAllErrors().stream().map(erro -> erro.getDefaultMessage()).collect(Collectors.toList());
+        return new ApiErrors(errors);
     }
 }
