2dc78d9a126eb26c43e43f383487b13994151e00
Luana Melissa Linux <luanamelissaprogrammer@gmail.com>
2022-08-31 20:47:12 -0300

    Configuração para armazenamento de dados em memória

--- src/main/java/io/github/luaprogrammer/config/SecurityConfig.java
+++ src/main/java/io/github/luaprogrammer/config/SecurityConfig.java
@@ -1,2 +1,58 @@
-package io.github.luaprogrammer.config;public class SecurityConfig {
+package io.github.luaprogrammer.config;
+
+import io.github.luaprogrammer.service.impl.UsuarioServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+//    private UsuarioServiceImpl usuarioService;
+//
+//    public SecurityConfig(UsuarioServiceImpl usuarioService) {
+//        this.usuarioService = usuarioService;
+//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated().and() //volta para a raiz do objeto
+                .csrf().disable(); //desabilita a proteção csrf
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(passwordEncoder().encode("userPass"))
+                .roles("USER")
+                .build());
+        manager.createUser(User.withUsername("admin")
+                .password(passwordEncoder().encode("adminPass"))
+                .roles("USER", "ADMIN")
+                .build());
+        return manager;
+    }
 }
